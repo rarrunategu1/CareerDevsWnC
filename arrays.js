8b89766a6c4928d825b74cd6e4dbe3c82a71829e
rose parker <parker.rose@intracitygeeks.org>
2017-07-13 01:45:54 +0000

    V1 Requirement - place to store todos

---
+++ arrays.js
@@ -0,0 +1,17 @@
+['item 1', 'item 2', 'item 3']
+//An array is a "list" in programming. Every item in an array
+//should have quotation marks and the list should start and end in a bracket.
+//Pressing enter from here will give you the array which should be the following:
+
+["item 1", "item 2", "item 3"]
+//this array is stored somewhere but it's kind of lost.
+//To get it back we need to give the array a "nickname" as you'll see below:
+
+var todos = ["item 1", "item 2", "item 3"]
+//The "nickname for this array of items is "todos".  To create the nickname
+//give it the prefix var and after the nickname put the equal sign.  This way
+//when you type in todos, it will bring up the array of items as you'll see below:
+
+todos 
+//will return
+["item 1", "item 2", "item 3"]
