5f9dc12bad10b53f078f84b3b969d2810c17e4ab
rose parker <parker.rose@intracitygeeks.org>
2017-07-14 00:16:45 +0000

    Functions are like recipes-Turkey sandwich

---
+++ functions.js
@@ -0,0 +1,25 @@
+//FUNCTIONS ARE JUST RECIPES
+
+makeTurkeySandwich
+    Get one slice of bread.
+    Add turkey.  
+    Put a slice of bread on top.  
+    
+// to turn this recipe into a function add the following:
+// note that all the periods became semi colons
+// remember parenthesis first then "brace" your hands around the ingredients
+function makeTurkeySandwich() {
+    Get one slice of bread;
+    Add turkey; 
+    Put a slice of bread on top;  
+}
+
+
+// to make the turkey sandwich or run the function do the following
+// to run all the steps in your function.  
+makeTurkeySandwich()
+
+
+
+
+    
