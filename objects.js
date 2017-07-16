3a7c51bdca0739a4c93be07103d3681487ecb154
rose parker <parker.rose@intracitygeeks.org>
2017-07-16 20:24:44 +0000

    objects and functions

--- objects.js
+++ objects.js
@@ -42,3 +42,76 @@ myComputer.screenSize
 "15 inches"
 
 
+//OBJECTS AND FUNCTIONS
+
+
+//You can place function on objects
+
+{
+    name: 'rose'
+    sayName: function() {
+    console.log();    
+    }
+}
+
+//above object represents a person.  
+//namng a property that's a function is seen on sayName: function(){}
+//difference on the sayName function is that in an object, you don't need to
+//have the name of the function.  It's optional  So you can leave it as function
+//which you see above.
+
+//To represent the object that you're on you can use "this
+//when using keyword "this" inside of a function that is on an object
+//"this" will refer to that object
+{
+    name: 'rose'
+    sayName: function() {
+    console.log(this);    
+    }
+}
+
+//running this will print out the entire object.
+//First we'll name it with a variable.
+var rose = {
+    name: 'rose'
+    sayName: function() {
+    console.log(this);    
+    }
+}
+
+//then type:
+rose.sayName()
+//It will print out the entire object
+Object {name: "rose"}
+    name: "rose"
+    sayName: function ()
+    _proto_: Object 
+    
+//in order for it to not print the entire object and for it to just print the name
+
+var rose = {
+    name: 'rose'
+    sayName: function() {
+    console.log(this.name);    
+    }
+}
+
+//Hit enter and do the following:
+rose.sayName()
+//which will print out 
+rose
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
