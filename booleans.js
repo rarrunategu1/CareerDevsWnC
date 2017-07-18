d5f07ab696509119c05ae3e3cff4ad867bf7f6bf
rose parker <parker.rose@intracitygeeks.org>
2017-07-18 10:57:35 +0000

    todoList.changeTodo should change the todoText property

--- booleans.js
+++ booleans.js
@@ -1,4 +1,4 @@
-//todoList.addTodo should add objects
+//todoList.addTodo should add objects to our array
 
 //instead of adding an array of text, it will add an array of objects
 //we are doing this because we want to represent more data and an object
@@ -46,6 +46,61 @@ todoList.addTodo('this is an object');
 //which would return:
 VM829 script.js:4 My Todos [Object]0: Objectcompleted: false todoText: "this is an object"__proto__: Objectlength: 1__proto__: Array(0)
 undefined
+
+
+//todoList.changeTodo should change the todoText property
+
+
+
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    console.log('My Todos', this.todos);
+  },
+  addTodo: function(todoText) {// addTodo('hi') todoText: 'hi',
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText; //this.todos[position] is an object;
+    //because this is a todo object from the addTodo object we can get to a specific property using the .todoText 
+    this.displayTodos();
+  },
+  // deleteTodo: function(position) {
+  //   this.todos.splice(position, 1);
+  //   this.displayTodos();
+    
+  // }
+  
+};
+
+//enter the code in a console, press enter and addTodo like so
+todoList.addTodo('first try');
+//it will return
+VM435:4 My Todos [Object]0: Objectcompleted: falsetodoText: "first try"__proto__: Objectlength: 1__proto__: Array(0)
+undefined
+//change like below:
+todoList.changeTodo(0, 'second try');
+//and it will return
+VM435:4 My Todos [Object]0: Objectcompleted: falsetodoText: "second try"__proto__: Objectlength: 1__proto__: Array(0)
+undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
