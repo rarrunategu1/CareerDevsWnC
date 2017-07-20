ce0ec8ec967986e927fa92b0e1c541e67dbf09b1
rose parker <parker.rose@intracitygeeks.org>
2017-07-20 21:57:27 +0000

    if everything's true, make everything false

---
+++ think-in-code.js
@@ -0,0 +1,51 @@
+//.toggleAll: if everything's true, make everything false
+
+toggleAll: function() {
+    
+    var totalTodos === this.todos.length;
+    var completedTodos = 0;
+    
+    //Get number of completed todos.
+    for (var i=0; i<totalTodos; i++) {
+        if (this.todos[i].complete === true) {
+            completedTodos++;
+        }
+    }
+   
+   //if everything's true, make everything false
+ 
+    if(completedTodos === totalTodos) {
+        //make everything false.
+        for (var i=0; i<totalTodos; i++) {
+            this.todos[i].completed = false;
+        }
+        
+//the completed code looks like this and it will ONLY WORK IF ALL THE TODOS ARE COMPLETED!
+
+toggleAll: function() {
+    var totalTodos === this.todos.length;
+    var completedTodos = 0;
+    
+    //Get number of completed todos.
+    for (var i=0; i<totalTodos; i++) {
+        if (this.todos[i].completed === true) {
+            completedTodos++;
+        }
+    } //if everything is true make everything false:
+    if(completedTodos === totalTodos) {
+        for (var i=0; i<totalTodos; i++) {
+            this.todos[i].completed = false;
+        }
+    }
+    
+    this.displayTodos();
+    
+        
+        
+        
+        
+        
+        
+        
+    }
+}
