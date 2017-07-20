c0c1edba2d5a6b4be72c45d91638f4f2868f48a1
rose parker <parker.rose@intracitygeeks.org>
2017-07-20 19:33:03 +0000

    the for loop

---
+++ loops.js
@@ -0,0 +1,24 @@
+//the for loop
+
+i=0 //initialization
+say "hey" if i<3 //condition if true it will keep giving until it's false
+increase i by 1 //final expression
+
+0 "hey"
+1 "hey"
+2 "hey"
+3
+
+//this is how you would accomplish if you were talking to the computer in english
+
+//in code it works like this:
+
+for (initialization, condition, final expression) {
+    console.log("hey");
+}
+
+//which will be the following for the code:
+
+for (var i=0; i<3; i++) {
+    console.log("hey");
+}
