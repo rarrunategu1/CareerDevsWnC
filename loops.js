814258814f1ba1233107d5ee98b853db2a49d9c4
rose parker <parker.rose@intracitygeeks.org>
2017-07-20 20:02:05 +0000

    looping over arrays

--- loops.js
+++ loops.js
@@ -22,3 +22,49 @@ for (initialization, condition, final expression) {
 for (var i=0; i<3; i++) {
     console.log("hey");
 }
+
+//LOOPING OVER ARRAYS
+
+for (var i=0; i<3; i++) {
+    console.log(i);
+}
+
+//if you console log i it will print 
+//0
+//1
+//2
+
+
+for (var i=0; i<3; i++) {
+    console.log("hey");
+}
+
+//will return 
+//hey
+//hey
+//hey
+
+var testArray = ['item 1', 'item 2', 'item 3'];
+
+testArray[0] //returns item 1
+testArray[1] //returns item 2
+
+//you can this to your advantage inside of our for loop
+
+for (var i=0; i<3; i++) {
+    console.log(testArray[i]);
+}
+
+//and it will return as the variable i will access all the different items in the array
+//item 1
+//item 2
+//item 3
+
+//you can also take the condition i<3 and change it to the following (still gives same result), which is better to use
+
+for (var i=0; testArray.length; i++) {
+    console.log(testArray[i]);
+}
+
+//this is better in case you have more than 3 Arrays, for example if you add one, it will just go with the length of the array, not limiting it.
+
