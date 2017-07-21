dbfc9a8b88eb67926d968b0a99257f2da1695834
rose parker <parker.rose@intracitygeeks.org>
2017-07-21 00:49:23 +0000

    Computer Science Names for Data Types - Primitives and Objects

---
+++ data-types.js
@@ -0,0 +1,23 @@
+//COMPUTER SCIENCE NAMES FOR DATA TYPES
+
+//two types - Objects an Primitives
+
+//OBJECTS (can be as complex as you want)
+//{} - objects are in curly braces // todoList, arrays, functions
+
+
+//PRIMITIVES (building blocks)
+String  // 'Look at this string!'  //string is the formal name for text
+Number  // 1, 2, 3, 4 ...
+Boolean // true, false
+Undefined   // value that hasn't been set
+Null        // 'Nothing'   //this is good to use if there is a test that hasn't been graded, then it's nul
+
+
+// if it doesn't look like a primitive value then it will be an object
+
+
+
+
+
+
