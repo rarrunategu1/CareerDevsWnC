626d24f38917a0313f72f77e1cf2e2f43fadb1df
rose parker <parker.rose@intracitygeeks.org>
2017-07-23 23:53:49 +0000

    clicking display todos should run todolist.displaytodos

--- html-and-the-dom.js
+++ html-and-the-dom.js
@@ -4,8 +4,16 @@
 //THERE SHOULD BE 'DISPLAY TODOS' BUTTON AND A 'TOGGLE ALL' BUTTON IN THE APP.
  
  
+//CLICKING "DISPLAY TODOS" SHOULD RUN TODOLIST.DISPLAYTODOS
+//we want to get access to the display todos button.
+var displayTodosButton = document.getElementById('displayTodosButton');
+console.log(displayTodosButton);
 
-
+//we want to run displayTodos method, when someone clicks the display todos button.
+displayTodosButton.addEventListener('click', function()
+{
+    todoList.displayTodos();
+});
 
 
 
