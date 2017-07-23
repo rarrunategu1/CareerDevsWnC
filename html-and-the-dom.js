e52e9eb69731c63e8674f543534ca2cb4a99a473
rose parker <parker.rose@intracitygeeks.org>
2017-07-23 23:11:39 +0000

    There should be a display todos and a toggle all button in the app

---
+++ html-and-the-dom.js
@@ -0,0 +1,16 @@
+//The DOM is The browser's interpretation of your HTML
+//We're only going to use it for javascript
+
+//THERE SHOULD BE 'DISPLAY TODOS' BUTTON AND A 'TOGGLE ALL' BUTTON IN THE APP.
+ 
+ 
+
+
+
+
+
+
+
+
+
+
