f7db544a7ef6e85152fe3486d74bb4b1aba3337c
rose parker <parker.rose@intracitygeeks.org>
2017-07-30 16:38:32 +0000

    forEach

--- functions-interlude.js
+++ functions-interlude.js
@@ -31,4 +31,90 @@ console.log('Wake up Rose!');
  Wake up Rose!
 
 
+//forEach
+
+//in console enter
+var students = ['jonathan', 'jenny', 'elliot'];
+//returns 
+undefined
+//in console enter
+function logName(name) {
+console.log(name);
+}
+//returns
+undefined
+//if I want to run logName over every student under the array I'd the following
+//but it's alot of work and it's very manual.  
+
+logName(students[0])
+//returns
+jonathan
+undefined
+
+//to make it easier and not have to do it for every student you'd do a for loop
+
+//in console enter
+for (var i=0; i < students.length; i++) {
+logName(students[i]);
+}
+//it returns
+jonathan
+jenny
+elliot
+undefined
+
+//but theres an easier way to do it to make it even shorter
+
+//all you do is pass in a function and it'll run that for loop for you on every item in the array
+students.forEach(logName)
+//returns
+ jonathan
+ jenny
+ elliot
+undefined
+//or if you're entering the function with it if it wasn't previously entered
+students.forEach(function logName(name) {
+console.log(name);
+})
+//returns
+ jonathan
+ jenny
+ elliot
+undefined
+//or you can make the function with no name
+students.forEach(function(name) {
+console.log(name);
+})
+//returns
+ jonathan
+ jenny
+ elliot
+undefined
+
+//lets create our own forEAch
+//this for each is stand alone, it is not a method on an array
+//so you have to pass in an array(myArray) and a function (myFunction)
+
+function forEach(myArray, myFunction) {
+for (var i=0; i < myArray.lengeth; i++) {
+myFunction(myArray[i]);
+}
+}
+//returns
+undefined
+//enter in console
+forEach(students, function(student) {
+console.log(student);
+})
+//will return all the names as well
+
+//next time you call it you can do 
+forEach(students, logName)
+//which also returns the names
+
+
+
+
+
+
 
