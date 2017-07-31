90d1f79125f8a7969e1c8fc5c7989b35486ae9b2
rose parker <parker.rose@intracitygeeks.org>
2017-07-31 00:49:26 +0000

    todoList.toggleAll should use forEach

---
+++ destroy-forloops.js
@@ -0,0 +1,169 @@
+// var todoList = {
+//   todos: [],
+//   addTodo: function(todoText) {
+//     this.todos.push({
+//       todoText: todoText,
+//       completed: false
+//     });
+//   },
+//   changeTodo: function(position, todoText) {
+//     this.todos[position].todoText = todoText;
+//   },
+//   deleteTodo: function(position) {
+//     this.todos.splice(position, 1);
+//   },
+//   toggleCompleted: function(position) {
+//     var todo = this.todos[position];
+//     todo.completed = !todo.completed;
+//   },
+//   toggleAll: function() {
+//     var totalTodos = this.todos.length;
+//     var completedTodos = 0;
+    
+//     // Get number of completed todos.
+//     for (var i = 0; i < totalTodos; i++) {
+//       if (this.todos[i].completed === true) {
+//         completedTodos++;
+//       }
+//     }
+//     // Case 1: If everything’s true, make everything false.
+//     if (completedTodos === totalTodos) {
+//       for (var i = 0; i < totalTodos; i++) {
+//         this.todos[i].completed = false;
+//       }
+//     // Case 2: Otherwise, make everything true.
+//     } else {
+//       for (var i = 0; i < totalTodos; i++) {
+//         this.todos[i].completed = true;
+//       }      
+//     }
+//   }
+// };
+
+// var handlers = {
+//   addTodo: function() {
+//     var addTodoTextInput = document.getElementById('addTodoTextInput');
+//     todoList.addTodo(addTodoTextInput.value);
+//     addTodoTextInput.value = '';
+//     view.displayTodos();
+//   },
+//   changeTodo: function() {
+//     var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+//     var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+//     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+//     changeTodoPositionInput.value = '';
+//     changeTodoTextInput.value = '';
+//     view.displayTodos();
+//   },
+//   deleteTodo: function(position) {
+//     todoList.deleteTodo(position);
+//         view.displayTodos();
+//   },
+//   toggleCompleted: function() {
+//     var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+//     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+//     toggleCompletedPositionInput.value = '';
+//     view.displayTodos();
+//   },
+//   toggleAll: function() {
+//     todoList.toggleAll();
+//     view.displayTodos();
+//   }  
+// };
+
+// var view = {
+//   displayTodos: function() {
+//     var todosUl = document.querySelector('ul');
+//     todosUl.innerHTML = '';
+//     for (var i = 0; i < todoList.todos.length; i++) {
+//       var todoLi = document.createElement('li');
+//       var todo = todoList.todos[i];
+//       var todoTextWithCompletion = '';
+
+//       if (todo.completed === true) {
+//         todoTextWithCompletion = '(x) ' + todo.todoText;
+//       } else {
+//         todoTextWithCompletion = '( ) ' + todo.todoText;
+//       }
+      
+//       todoLi.id = i;
+//       todoLi.textContent = todoTextWithCompletion;
+//       todoLi.appendChild(this.createDeleteButton());
+//       todosUl.appendChild(todoLi);
+//     }
+//   },
+//   createDeleteButton: function() {
+//     var deleteButton = document.createElement('button');
+//     deleteButton.textContent = 'Delete';
+//     deleteButton.className = 'deleteButton';
+//     return deleteButton;
+//   },
+  
+//   setUpEventListeners: function() {
+//     var todosUl = document.querySelector('ul');
+
+// todosUl.addEventListener('click', function (event) {
+ 
+//   //get the element that was clicked on.
+//   var elementClicked = event.target;
+//   //check if the elementClicked is a delete button
+//   if(elementClicked.className === 'deleteButton') {
+//   //run handlers.deleteTodo(position)
+//   handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+//   }
+//   });
+//  }
+// };
+
+// view.setUpEventListeners();
+
+
+
+//NOTES:lines 23 thru 41 become
+
+    // Get number of completed todos.
+this.todos.forEach(function(todo) {
+  if(todo.completed === true) {
+    completedTodos++;
+  }
+})
+    
+    // Case 1: If everything’s true, make everything false.
+    if (completedTodos === totalTodos) {
+      this.todos.forEach(function(todo) {
+                  todo.completed = false;       
+                         });
+    // Case 2: Otherwise, make everything true.
+    } else {
+      this.todos.forEach(function(todo) {
+        todo.completed = true;
+      });     
+    }
+  }
+};
+
+//BUT TO MAKE IT MORE SIMPLE, DELETE IT ALL AND REPLACE WITH
+ 
+  // Get number of completed todos.
+    this.todos.forEach (function(todo) {
+      if (todo.completed === true) {
+        completedTodos++;
+      }
+    });
+
+     this.todos.forEach(function(todo) {
+  //case 1: If everything's true, make everything false.
+       if (completedTodos === totalTodos) {
+         todo.completed = false;
+            //Case 2: otherwise make everything true
+         } else {
+           todo.completed = true;
+         }
+    });
+  }
+};
+
+//this.todos was removed
+
+
+
